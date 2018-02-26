eb2b8c9ded8b461f54b55c407aaa1af02a034e23
titetil <gtetil@us.tiauto.com>
2018-02-26 07:13:45 -0500

    - Made several modifications.  First version was a mess.

--- Evap_Temp_Alarm.ino
+++ Evap_Temp_Alarm.ino
@@ -4,11 +4,11 @@ int mutePin = 8;
 int alarmPin = 9;
 int muteBtnState = 0;
 int alarmState = 0;
-int prevAlarmState = 0;
+int prevAlarmState = 1;
 int prevMuteState = 0;
-int muteTime = 10000; //in milliseconds
-int muteTimer;
-int muteTimerElapsed = 0;
+int long muteTime = 300000; //in milliseconds
+int long muteTimer = millis();
+int long muteTimerElapsed = 0;
 int muteState = 0;
 
 void setup() {
@@ -16,6 +16,7 @@ void setup() {
   pinMode(rlyPin, OUTPUT);
   pinMode(mutePin, INPUT_PULLUP);
   pinMode(alarmPin, INPUT_PULLUP);
+  //Serial.begin(9600);
 
 }
 
@@ -23,10 +24,14 @@ void loop() {
 
   muteBtnState = digitalRead(mutePin);
   alarmState = digitalRead(alarmPin);
+  //delay(500);
+  //Serial.println(muteBtnState);
 
   //check if mute button was pushed
-  if (muteBtnState == 1 && prevMuteState == 0) {
+  if (muteBtnState == 1) {
     muteTimer = millis();
+    setRelay(0);
+    muteState = 1;
   }
 
   //mute state is "on" if still within mute time
@@ -34,18 +39,22 @@ void loop() {
   if (muteTimerElapsed > muteTime) {
     muteState = 0;
   }
-  else {
-    muteState = 1;
-  }
 
+  if (alarmState == 1 && prevAlarmState == 0) {
+    muteTimer = 0;
+    muteState = 0;
+  };
+  
   //turn on relay if alarm is active and not muted
   if (alarmState == 1 && muteState == 0) {
     setRelay(1);
   }
-  else {
+
+  if (alarmState == 0) {
     setRelay(0);
   }
 
+  prevAlarmState = alarmState;
 }
 
 void setRelay(int state) {
